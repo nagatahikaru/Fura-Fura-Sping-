dcea81b0dcaccaf2a3a052be53477c9a6216d33f
KawaharaTakagi <CA01254011@st.kawahara.ac.jp>
2026-03-16 12:05:06 +0900

    バットのUI追加

--- ふらふら☆スピング/Game/Source/UI/InGameUI/InGameUI.cpp
+++ ふらふら☆スピング/Game/Source/UI/InGameUI/InGameUI.cpp
@@ -3,6 +3,7 @@
 
 InGameUI::InGameUI() {
 	m_spriteRender.Init("Assets/sprite/waku.DDS", 600.0f, 400.0f);
+	m_spriteRenderBat.Init("Assets/sprite/batto.DDS", 200.0f, 300.0f);
 }
 
 InGameUI::~InGameUI() {
@@ -10,6 +11,7 @@ InGameUI::~InGameUI() {
 }
 
 bool InGameUI::Start() {
+	m_batPosition = Vector3{0.0f, -100.0f, 0.0f };  // ← 初期位置
 	return true;
 }
 
@@ -17,8 +19,23 @@ void InGameUI::Update() {
 
 }
 
+//バットの位置を設定
+void InGameUI::SetBatPosition(const Vector3& pos) {
+	m_batPosition = pos;
+}
+
+//バットの回転を設定
+void InGameUI::SetBatRotation(float rotDeg) {
+	m_rad = rotDeg * 3.14159265f / 180.0f;
+	m_batRotation.SetRotation(Vector3::AxisZ, m_rad);
+}
+
 void InGameUI::Render(RenderContext& rc) {
-	m_spriteRender.SetPosition(Vector3{ 0.0f, -100.0f, 0.0f }); // 画面左上
+	m_spriteRender.SetPosition(Vector3{ 0.0f, -100.0f, 0.0f }); 
 	m_spriteRender.Update();
 	m_spriteRender.Draw(rc);
+	m_spriteRenderBat.SetPosition(m_batPosition);
+	m_spriteRenderBat.SetRotation(m_batRotation);
+	m_spriteRenderBat.Update();
+	m_spriteRenderBat.Draw(rc);
 }
--- ふらふら☆スピング/Game/Source/UI/InGameUI/InGameUI.h
+++ ふらふら☆スピング/Game/Source/UI/InGameUI/InGameUI.h
@@ -9,7 +9,13 @@ class InGameUI:public Source
 	virtual void Update();
 	virtual bool Start();
 	virtual void Render(RenderContext& rc);
+	void SetBatPosition(const Vector3&pos);
+	void SetBatRotation(float rotDeg);
 private:
 	SpriteRender m_spriteRender;
+	SpriteRender m_spriteRenderBat;
+	Vector3 m_batPosition = Vector3::Zero;
+	Quaternion m_batRotation = Quaternion::Identity;
+	float m_rad;
 };
 
