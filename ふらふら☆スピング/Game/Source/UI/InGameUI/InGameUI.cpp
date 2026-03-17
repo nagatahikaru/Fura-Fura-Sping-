6abcf4cbe684a042deea0e716023345e354d3fc9
KawaharaTakagi <CA01254011@st.kawahara.ac.jp>
2026-03-17 15:03:51 +0900

    各カメラのUIの変更追加

--- ふらふら☆スピング/Game/Source/UI/InGameUI/InGameUI.cpp
+++ ふらふら☆スピング/Game/Source/UI/InGameUI/InGameUI.cpp
@@ -5,6 +5,7 @@ InGameUI::InGameUI() {
 	m_spriteRender.Init("Assets/sprite/waku.DDS", 600.0f, 400.0f);
 	m_spriteRenderBat.Init("Assets/sprite/batto.DDS", 200.0f, 300.0f);
 	m_spriteRenderMeet.Init("Assets/sprite/mi-to.DDS", 30.0f, 30.0f);
+	m_spriteRenderReplay.Init("Assets/sprite/REPLAY.DDS", 300.0f, 300.0f);
 }
 
 InGameUI::~InGameUI() {
@@ -33,35 +34,48 @@ void InGameUI::SetBatRotation(float rotDeg) {
 }
 
 void InGameUI::Render(RenderContext& rc) {
-	//赤い枠
-	m_spriteRender.SetPosition(Vector3{ 0.0f, -100.0f, 0.0f }); 
-	m_spriteRender.Update();
-	m_spriteRender.Draw(rc);
 
-	//バット
-	m_spriteRenderBat.SetPosition(m_batPosition);
-	m_spriteRenderBat.SetRotation(m_batRotation);
-	m_spriteRenderBat.Update();
-	m_spriteRenderBat.Draw(rc);
+	if (m_isUIVisible) {
 
-	// --- ミートゾーン（バットと同じ位置・回転を参照） ---
-	m_meetPos = m_batPosition + m_meetPosition;
-	m_spriteRenderMeet.SetPosition(m_meetPos);
-	m_spriteRenderMeet.SetRotation(m_batRotation);
-	m_spriteRenderMeet.Update();
-	m_spriteRenderMeet.Draw(rc);
+		//赤い枠
+		m_spriteRender.SetPosition(Vector3{ 0.0f, -100.0f, 0.0f });
+		m_spriteRender.Update();
+		m_spriteRender.Draw(rc);
 
-	wchar_t text[256];
-	swprintf_s(text, L"ぐるぐる:%d", 0);
-	m_fontRender.SetText(text);
-	m_fontRender.SetPosition(-900.0f,500.0f,0.0f);
-	m_fontRender.SetColor(0.0f, 0.78f, 0.31f, 1.0f);
-	m_fontRender.Draw(rc);
+		//バット
+		m_spriteRenderBat.SetPosition(m_batPosition);
+		m_spriteRenderBat.SetRotation(m_batRotation);
+		m_spriteRenderBat.Update();
+		m_spriteRenderBat.Draw(rc);
 
-	wchar_t boll[256];
-	swprintf_s(boll,L"%dkm",0);
-	m_fontBollRender.SetText(boll);
-	m_fontBollRender.SetPosition(800.0f, 500.0f, 0.0f);
-	m_fontBollRender.SetColor(0.0f, 0.0f, 0.0f, 1.0f);
-	m_fontBollRender.Draw(rc);
+		// --- ミートゾーン（バットと同じ位置・回転を参照） ---
+		m_meetPos = m_batPosition + m_meetPosition;
+		m_spriteRenderMeet.SetPosition(m_meetPos);
+		m_spriteRenderMeet.SetRotation(m_batRotation);
+		m_spriteRenderMeet.Update();
+		m_spriteRenderMeet.Draw(rc);
+
+	}
+
+	if (m_isFontVisible) {
+		wchar_t text[256];
+		swprintf_s(text, L"ぐるぐる:%d", 0);
+		m_fontRender.SetText(text);
+		m_fontRender.SetPosition(-900.0f, 500.0f, 0.0f);
+		m_fontRender.SetColor(0.0f, 0.78f, 0.31f, 1.0f);
+		m_fontRender.Draw(rc);
+
+		wchar_t boll[256];
+		swprintf_s(boll, L"%dkm", 0);
+		m_fontBollRender.SetText(boll);
+		m_fontBollRender.SetPosition(800.0f, 500.0f, 0.0f);
+		m_fontBollRender.SetColor(0.0f, 0.0f, 0.0f, 1.0f);
+		m_fontBollRender.Draw(rc);
+	}
+
+	if (m_isReplayVisible) {
+		m_spriteRenderReplay.SetPosition(Vector3{ -800.0f, 450.0f, 0.0f });
+		m_spriteRenderReplay.Update();
+		m_spriteRenderReplay.Draw(rc);
+	}
 }
--- ふらふら☆スピング/Game/Source/UI/InGameUI/InGameUI.h
+++ ふらふら☆スピング/Game/Source/UI/InGameUI/InGameUI.h
@@ -11,17 +11,24 @@ class InGameUI:public Source
 	virtual void Render(RenderContext& rc);
 	void SetBatPosition(const Vector3&pos);
 	void SetBatRotation(float rotDeg);
+	void SetUIVisible(bool isVisible) { m_isUIVisible = isVisible; }
+	void SetFontVisble(bool isVisible) { m_isFontVisible = isVisible; }
+	void SetReplayVisible(bool isVisible){ m_isReplayVisible = isVisible; }
 private:
 	FontRender m_fontRender;
 	FontRender m_fontBollRender;
 	SpriteRender m_spriteRender;
 	SpriteRender m_spriteRenderBat;
 	SpriteRender m_spriteRenderMeet;
+	SpriteRender m_spriteRenderReplay;
 	Vector3 m_batPosition = Vector3::Zero;
 	Vector3 m_meetPosition = Vector3::Zero;
 	Quaternion m_batRotation = Quaternion::Identity;
 	float m_rad;
 	Vector3 m_meetPos;
 	float m_km;
+	bool m_isUIVisible = true;
+	bool m_isFontVisible = true;
+	bool m_isReplayVisible = true;
 };
 
