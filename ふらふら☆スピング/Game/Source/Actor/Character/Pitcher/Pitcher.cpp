b301cc0be8f5e9bcbdaa559f6d502384837afa91
iwamotorikuto <ca01254003@st.kawahara.ac.jp>
2026-03-31 10:30:02 +0900

    Pitcherの位置変更

--- ふらふら☆スピング/Game/Source/Actor/Character/Pitcher/Pitcher.cpp
+++ ふらふら☆スピング/Game/Source/Actor/Character/Pitcher/Pitcher.cpp
@@ -102,7 +102,7 @@ bool Pitcher::Start()
 		&m_modelRender[m_UniformNumber],
 		m_animationClips,
 		enAnimationClip_Num,
-		Vector3(-150.0f, -25.0f, 1000.0f),
+		Vector3(-0.0f, -5.0f, 1000.0f),
 		Vector3(10.0f, 10.0f, 10.0f),
 		GetBatterUniformNumberFilePath(m_UniformNumber));
 
