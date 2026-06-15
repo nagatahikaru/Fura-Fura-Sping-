6da00aa8574c03de20a9b2060c5aa33bd21a618f
01254016 <ca01254016@st.kawahara.ac.jp>
2026-06-15 09:35:03 +0900

    演出クラスとパターンの削除、デバフバグの解消、コードの整理（Batter）、検証モデル・テクスチャの削除、それに伴うコードの修正

--- ふらふら☆スピング/Game/Source/Actor/Character/Pitcher/Pitcher.cpp
+++ ふらふら☆スピング/Game/Source/Actor/Character/Pitcher/Pitcher.cpp
@@ -3,69 +3,7 @@
 #include"Source/Scene/InGame/Game.h"
 #include "Source/Actor/Character/Batter/Batter.h"
 
-namespace {
-	std::string FILE_PATH_BATTER = ("Assets/animData/pitcher/");
-	std::string FILE_PATH_BATTER_UNIFORMNUMBER = ("Assets/modelData/Pitcher/UniformNumber/");
-	std::string FILE_PATH_TKM = (".tkm");
-	std::string FILE_PATH_DDS = (".tka");
-	std::string FILE_PATH_NUMBER[1] = {
-		"0"
-	};
 
-	std::string FILE_PATH_ANIMATION[2] = {
-		"Idle",
-		"Throw"
-	};
-
-	inline std::string GetBatterUniformNumberFilePath(int number)
-	{
-		return FILE_PATH_BATTER_UNIFORMNUMBER + FILE_PATH_NUMBER[number] + FILE_PATH_TKM;
-	}
-
-	inline std::string GetAnimationFilePath(int number)
-	{
-		return FILE_PATH_BATTER + FILE_PATH_ANIMATION[number] + FILE_PATH_DDS;
-	}
-
-	/**
-	モデルの一括初期化処理
-	ModelRenderの初期化、位置、スケール設定、更新処理をまとめて行う関数
-	modelRender			 初期化するModelRenderのポインタ
-	m_animationClips	 アニメーションクリップの配列
-	enAnimationClip_Num	 アニメーションクリップの数
-	pos					 モデルの位置
-	scl					 モデルのスケール
-	filePath			 モデルデータのファイルパス
-	例:
-	InitModelRender(
-		&m_modelRender[i],
-		m_animationClips,
-		enAnimationClip_Num,
-		PlayerVariable::Transform::INITIAL_COORDINATE,
-		PlayerVariable::Transform::INITIAL_SCALE,
-		GetModelFilePath(i));
-	*/
-	void InitModelRender(
-		ModelRender* modelRender
-		, AnimationClip* m_animationClips
-		, int enAnimationClip_Num
-		, const Vector3& pos
-		, const Vector3& scl
-		, std::string filePath) {
-		modelRender->Init(filePath.c_str(), m_animationClips, enAnimationClip_Num, enModelUpAxisZ);
-		modelRender->SetPosition(pos);
-		modelRender->SetScale(scl);
-		modelRender->Update();
-	}
-
-	void InitAnimation(AnimationClip animation[], int number, bool loop)
-	{
-		animation[number].Load(GetAnimationFilePath(number).c_str());
-		animation[number].SetLoopFlag(loop);
-	}
-
-
-}
 
 Pitcher::Pitcher()
 {
@@ -82,22 +20,22 @@ bool Pitcher::Start()
 	{
 		if (j == enAnimationClip_Throw)
 		{
-			InitAnimation(m_animationClips, j, false);
+			PITCHER::InitAnimation(m_animationClips, j, false);
 		}
 		else
 		{
-			InitAnimation(m_animationClips, j, true);
+			PITCHER::InitAnimation(m_animationClips, j, true);
 		}
 	}
 
 	//初期位置の設定
-	InitModelRender(
+	PITCHER::InitModelRender(
 		&m_modelRender[m_UniformNumber],
 		m_animationClips,
 		enAnimationClip_Num,
-		Vector3(-0.0f, -5.0f, 1000.0f),
+		Vector3(-0.0f, 140.0f, 1000.0f),
 		Vector3(10.0f, 10.0f, 10.0f),
-		GetBatterUniformNumberFilePath(m_UniformNumber));
+		PITCHER::GetBatterUniformNumberFilePath(m_UniformNumber));
 
 
 	//アニメーション再生
--- ふらふら☆スピング/Game/Source/Actor/Character/Pitcher/Pitcher.h
+++ ふらふら☆スピング/Game/Source/Actor/Character/Pitcher/Pitcher.h
@@ -8,6 +8,69 @@ namespace {
 			Num
 		};
 	}
+	namespace PITCHER{
+		std::string FILE_PATH_PITCHER = ("Assets/animData/pitcher/");
+		std::string FILE_PATH_BATTER_UNIFORMNUMBER = ("Assets/modelData/Pitcher/UniformNumber/");
+		std::string FILE_PATH_TKM = (".tkm");
+		std::string FILE_PATH_DDS = (".tka");
+		std::string FILE_PATH_NUMBER[1] = {
+			"0"
+		};
+
+		std::string FILE_PATH_ANIMATION[2] = {
+			"Idle",
+			"Throw"
+		};
+
+		inline std::string GetBatterUniformNumberFilePath(int number)
+		{
+			return FILE_PATH_BATTER_UNIFORMNUMBER + FILE_PATH_NUMBER[number] + FILE_PATH_TKM;
+		}
+
+		inline std::string GetAnimationFilePath(int number)
+		{
+			return FILE_PATH_PITCHER + FILE_PATH_ANIMATION[number] + FILE_PATH_DDS;
+		}
+
+		/**
+		モデルの一括初期化処理
+		ModelRenderの初期化、位置、スケール設定、更新処理をまとめて行う関数
+		modelRender			 初期化するModelRenderのポインタ
+		m_animationClips	 アニメーションクリップの配列
+		enAnimationClip_Num	 アニメーションクリップの数
+		pos					 モデルの位置
+		scl					 モデルのスケール
+		filePath			 モデルデータのファイルパス
+		例:
+		InitModelRender(
+			&m_modelRender[i],
+			m_animationClips,
+			enAnimationClip_Num,
+			PlayerVariable::Transform::INITIAL_COORDINATE,
+			PlayerVariable::Transform::INITIAL_SCALE,
+			GetModelFilePath(i));
+		*/
+		void InitModelRender(
+			ModelRender* modelRender
+			, AnimationClip* m_animationClips
+			, int enAnimationClip_Num
+			, const Vector3& pos
+			, const Vector3& scl
+			, std::string filePath) {
+			modelRender->Init(filePath.c_str(), m_animationClips, enAnimationClip_Num, enModelUpAxisZ);
+			modelRender->SetPosition(pos);
+			modelRender->SetScale(scl);
+			modelRender->Update();
+		}
+
+		void InitAnimation(AnimationClip animation[], int number, bool loop)
+		{
+			animation[number].Load(GetAnimationFilePath(number).c_str());
+			animation[number].SetLoopFlag(loop);
+		}
+
+
+	}
 };
 
 class Pitcher :public Character
@@ -36,7 +99,6 @@ public:
 private:
 	Vector3 m_position = Vector3::Zero;
 	Vector3 m_targetPosition = Vector3::Zero;
-
 	
 	AnimationClip m_animationClips[enAnimationClip_Num];
 
